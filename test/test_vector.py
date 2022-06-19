0042f0c30d9a1f6a3e5aeb76f6d61024311446ac
Felix Urlaub <felixurlaub@googlemail.com>
2022-06-19 16:27:42 +0200

    add test for vector addition

--- test/test_vector.py
+++ test/test_vector.py
@@ -1,5 +1,9 @@
-from essence_la import vector
+from essence_la.vector import Vector
 
 def test_import_vector_class():
     vec = Vector([1,1])
 
+def test_add_vectors():
+    vec1 = Vector([1,3])
+    vec2 = Vector([2,-1])
+    assert vec1.add(vec2) == Vector([3,2])
