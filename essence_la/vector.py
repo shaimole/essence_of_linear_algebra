1ade23852131b49349010a88644e0b6eeb481866
Felix Urlaub <felixurlaub@googlemail.com>
2022-06-20 10:44:40 +0200

    add implementation for vector addition

--- essence_la/vector.py
+++ essence_la/vector.py
@@ -1,3 +1,8 @@
 class Vector:
     def __init__(self,elements):
-        self.elements = elements;
+        self.elements = elements
+
+    def add(self,vector):
+        sum  = map( lambda a,b: a+b,self.elements, vector.elements)
+        return self.__class__(list(sum))
+        
--- test/test_vector.py
+++ test/test_vector.py
@@ -6,4 +6,5 @@ def test_import_vector_class():
 def test_add_vectors():
     vec1 = Vector([1,3])
     vec2 = Vector([2,-1])
-    assert vec1.add(vec2) == Vector([3,2])
+    sum = vec1.add(vec2)
+    assert sum.elements == Vector([3,2]).elements
