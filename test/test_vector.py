f3236e0287b05ebec5799ad3cf9233af066f7339
unknown <felixurlaub@googlemail.com>
2022-07-31 21:09:16 +0200

    add tests for numpy version of vec operations

--- test/test_vector.py
+++ test/test_vector.py
@@ -12,8 +12,19 @@ def test_add_vectors():
     sum = vec1.add(vec2)
     assert sum == Vector([3, 2])
 
+def test_np_add_vectors():
+    vec1 = np.array([1, 3])
+    vec2 = np.array([2, -1])
+    sum = np.add(vec1, vec2)
+    assert sum.all() == np.array([3, 2]).all()
+
 def test_scale_vectors():
     vec = Vector([2.0, 1.1, 0.5])
     product = vec.scale(3)
     assert product == Vector([6.0, 3.3, 1.5]) 
+
+def test_np_scale_vectors():
+    vec = np.array([2.0, 1.1, 0.5])
+    product = np.multiply(vec, 3)
+    assert product.all() == np.array([6.0, 3.3, 1.5]).all()
 
