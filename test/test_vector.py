22e260711d4af6ce5304c6d5465a446bccc6aeef
unknown <felixurlaub@googlemail.com>
2022-07-31 09:55:40 +0200

    add equality to vector class itself

--- test/test_vector.py
+++ test/test_vector.py
@@ -7,4 +7,4 @@ def test_add_vectors():
     vec1 = Vector([1,3])
     vec2 = Vector([2,-1])
     sum = vec1.add(vec2)
-    assert sum.elements == Vector([3,2]).elements
+    assert sum == Vector([3,2])
