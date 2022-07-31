6f6df50841aa83123c3cdd9ca2a0901eba132f59
unknown <felixurlaub@googlemail.com>
2022-07-31 09:51:09 +0200

    add type hints

--- essence_la/vector.py
+++ essence_la/vector.py
@@ -1,8 +1,14 @@
+from numpy import void
+
+
 class Vector:
-    def __init__(self, elements):
+    def __init__(self, elements: list[float]):
         self.elements = elements
 
-    def add(self, vector):
+    def __str__(self) -> str:
+        return self.elements
+
+    def add(self, vector) -> 'Vector':
         add_elements = lambda a,b: a+b
         sum  = map(add_elements, self.elements, vector.elements)
         return Vector(list(sum))
