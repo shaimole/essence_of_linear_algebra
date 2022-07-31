c1161d09e1464e05611ecec1f7b25816aeb36ee3
unknown <felixurlaub@googlemail.com>
2022-07-31 09:55:54 +0200

    add equality to vector class itself

--- essence_la/vector.py
+++ essence_la/vector.py
@@ -8,6 +8,9 @@ class Vector:
     def __str__(self) -> str:
         return self.elements
 
+    def __eq__(self, other) -> bool:
+        return self.elements == other.elements
+
     def add(self, vector) -> 'Vector':
         add_elements = lambda a,b: a+b
         sum  = map(add_elements, self.elements, vector.elements)
