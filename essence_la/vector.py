a31eeba5edb6ece7dcafd75931f5ebe82b73a086
unknown <felixurlaub@googlemail.com>
2022-07-31 09:41:53 +0200

    refactor vector class

--- essence_la/vector.py
+++ essence_la/vector.py
@@ -1,8 +1,9 @@
 class Vector:
-    def __init__(self,elements):
+    def __init__(self, elements):
         self.elements = elements
 
-    def add(self,vector):
-        sum  = map( lambda a,b: a+b,self.elements, vector.elements)
-        return self.__class__(list(sum))
+    def add(self, vector):
+        add_elements = lambda a,b: a+b
+        sum  = map(add_elements, self.elements, vector.elements)
+        return Vector(list(sum))
         
