d96ed63332f7d0e40557809f3fc42808efc6b9d9
unknown <felixurlaub@googlemail.com>
2022-07-31 10:13:24 +0200

    add scale method

--- essence_la/vector.py
+++ essence_la/vector.py
@@ -6,13 +6,19 @@ class Vector:
         self.elements = elements
 
     def __str__(self) -> str:
-        return self.elements
+        return self.elements.__str__()
 
     def __eq__(self, other) -> bool:
         return self.elements == other.elements
 
-    def add(self, vector) -> 'Vector':
+    def add(self, vector: 'Vector') -> 'Vector':
         add_elements = lambda a,b: a+b
         sum  = map(add_elements, self.elements, vector.elements)
         return Vector(list(sum))
+
+    def scale(self, scalar: float) -> 'Vector':
+        scale_elements = lambda a: round(a*scalar, 15)
+        product = map(scale_elements, self.elements)
+        return Vector(list(product))
+
         
